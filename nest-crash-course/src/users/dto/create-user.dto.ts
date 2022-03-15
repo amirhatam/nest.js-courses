8712401ae22977c9073f4463800674292573dae7
Amir <amirhatam136@gmail.com>
2022-03-15 10:59:48 +0100

    add Validation decorators

--- nest-crash-course/src/users/dto/create-user.dto.ts
+++ nest-crash-course/src/users/dto/create-user.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsAlphanumeric, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  //Validation decorators
+  @IsAlphanumeric() //Checks if the string contains only letters and numbers.
+  @MaxLength(10) //Checks if the string's length is not more than given number.
   name: string;
 }
