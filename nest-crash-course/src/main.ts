377d0aa5e008c25cb2250efba2a1e6ab83054478
Amir <amirhatam136@gmail.com>
2022-03-15 10:43:05 +0100

    add useGlobalPipes & install class-validator class-transformer

--- nest-crash-course/src/main.ts
+++ nest-crash-course/src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //npm i class-validator class-transformer
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Nest API')
