9557bbf50270e5f8f58349ef751e2c24f4d6bc69
Amir <amirhatam136@gmail.com>
2022-03-16 11:02:50 +0100

    Add migrations config in ormconfig

--- typeorm/ormconfig.ts
+++ typeorm/ormconfig.ts
@@ -4,7 +4,13 @@ const config: SqliteConnectionOptions = {
   type: 'sqlite',
   database: 'db',
   entities: ['dist/src/**/*.entity.js'],
-  synchronize: true,
+  synchronize: false,
+  //Add typeorm command under scripts section in package.json:
+  //"typeorm": "node --require ts-node/register ./node_modules/typeorm/cli.js"
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
 };
 
 export default config;
